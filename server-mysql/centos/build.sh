7ad8e31f2214c469edb86eaaaa7f3500e2885ae7
zyongqing <zyongqing@gmail.com>
2017-03-14 17:38:17 +0800

    build server dockfile successful

--- server-mysql/centos/build.sh
+++ server-mysql/centos/build.sh
@@ -13,11 +13,4 @@ if [[ ! $version =~ ^[0-9]*\.[0-9]*\.[0-9]*$ ]] && [ "$version" != "latest" ]; t
     exit 1
 fi
 
-docker build -t zabbix-$app_component-$app_database:$os-$version -f Dockerfile .
-
-#docker rm -f zabbix-$app_component-$app_database
-#docker rm -f mysql-server
-
-#docker run --name mysql-server -t -e MYSQL_DATABASE="zabbix" -e MYSQL_USER="zabbix" -e MYSQL_PASSWORD="zabbix" -e MYSQL_RANDOM_ROOT_PASSWORD=true -d mysql:5.7
-#sleep 5
-#docker run --name zabbix-$app_component-$app_database -t -d --link mysql-server:mysql zabbix-$app_component-$app_database:$os-$version
+docker build -t aop-$app_component-$app_database:$os-$version -f Dockerfile .
